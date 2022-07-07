782e831f6349889dddf6fa9112d2173bb5aa1c6c
Whoppa <joeynalvarado@gmail.com>
2022-07-07 16:37:43 -0500

    text changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,12 +12,12 @@ const Home = () => {
           Joey Alvarado
         </h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
-          I'm a Full Stack Developer.
+          I'm a Full Stack Web3 Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I’m a full-stack developer specializing in building (and occasionally
-          designing) exceptional digital experiences. Currently, I’m focused on
-          building responsive full-stack web applications.
+          designing) exceptional digital experiences. Currently, I'm focused on
+          building responsive full-stack web3 web applications.
         </p>
         <div>
           <Link to="work" smooth={true} duration={500}>
