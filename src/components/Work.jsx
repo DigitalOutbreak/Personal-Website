237eeba9ea0f026ea851805bb9d21ee0523e6d00
Whoppa <joeynalvarado@gmail.com>
2022-09-16 06:24:00 -0500

    add nft collection

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -6,6 +6,7 @@ import CoinPrices from "../assets/coinprice.png";
 import MineSweeper from "../assets/minesweeper.png";
 import MidiPiano from "../assets/midipiano.png";
 import GoogleMaps from "../assets/googlemaps.png";
+import NftCollection from "../assets/nft-collection.png";
 
 const Work = () => {
   return (
@@ -41,19 +42,19 @@ const Work = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${CoinPrices})` }}
+            style={{ backgroundImage: `url(${NftCollection})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font-bold text-white tracking-wider">Crypto Coin Prices</span>
+              <span className="text-2xl font-bold text-white tracking-wider">NFT Collection</span>
               <div className="pt-8 text-center">
-                <a href="https://crypto-price-tracker-five.vercel.app/" target="blank">
+                <a href="https://nft-collection-one-lime.vercel.app/" target="blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/DigitalOutbreak/Crypto-Price-Tracker" target="blank">
+                <a href="https://github.com/DigitalOutbreak/NFT-Collection" target="blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
