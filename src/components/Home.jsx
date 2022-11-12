d63e348f2076f97285ceee8e13839dc7bc82ac54
Whoppa <joeynalvarado@gmail.com>
2022-11-12 10:38:55 -0600

    remove last name

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { HiArrowNarrowRight } from "react-icons/hi";
-import { Link } from "react-scroll";
+import React from 'react';
+import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
@@ -8,7 +8,7 @@ const Home = () => {
       {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-pink-600">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Joey Alvarado</h1>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">Joey</h1>
         <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">I'm a Front End Web Developer.</h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
           I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital
